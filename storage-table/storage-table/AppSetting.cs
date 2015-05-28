0ad9680cb4b8ccd021a517689fdaae2b81757b9e
sasingh <itssanjay@gmail.com>
2015-05-28 04:16:37 +0000

    inserts

---
+++ storage-table/storage-table/AppSetting.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.WindowsAzure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace storage_table
+{
+    public static class AppSetting
+    {
+        public static string StorageConnectionString
+        {
+            get
+            {
+                return CloudConfigurationManager.GetSetting("StorageConnectionString");
+            }
+        }
+    }
+}
--- storage-table/storage-table/Program.cs
+++ storage-table/storage-table/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,30 @@ namespace storage_table
     {
         static void Main(string[] args)
         {
+            TableFunctions tableFunctions = new TableFunctions();
+
+            Console.WriteLine("Creating table..");
+            TimedAction(()=>tableFunctions.CreateTable());
+
+            Console.WriteLine("Inserting 1000 rows");
+            TimedAction(()=>tableFunctions.BatchInsert(10));
+
+            Console.WriteLine("Performing 1000 selects..");
+            TimedAction(()=>tableFunctions.Select(10));
+
+            Console.WriteLine("Deleting table");
+            TimedAction(() => tableFunctions.DeleteTable());
+
+            Console.Read();
+        }
+
+        private static void TimedAction(Action action)
+        {
+            Stopwatch watch = new Stopwatch();
+            watch.Start();
+            action();
+            watch.Stop();
+            Console.WriteLine("Time Taken = " + watch.Elapsed.TotalSeconds);
         }
     }
 }
