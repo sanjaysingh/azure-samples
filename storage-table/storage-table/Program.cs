14a345b8b0b87d86623644213cd00531a21ca53f
sasingh <itssanjay@gmail.com>
2015-05-28 02:58:49 +0000

    initial

---
+++ storage-table/storage-table/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace storage_table
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
