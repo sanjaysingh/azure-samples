8dc2a6a288f885ab6f46c06f7f9664fc8bdbe6ad
Sanjay Singh <itssanjay@gmail.com>
2015-07-06 23:32:32 -0400

    Service Bus queue test

---
+++ ServiceBusTest/ServiceBusTest/SeerviceBusQueueTest.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.ServiceBus;
+using Microsoft.ServiceBus.Messaging;
+
+namespace ServiceBusTest
+{
+    [TestClass]
+    public class SeerviceBusQueueTest
+    {
+        private static QueueDescription testQueue;
+        private static NamespaceManager namespaceManager;
+        private QueueClient queueClient;
+
+        #region initializers
+
+        [ClassInitialize]
+        public static void ClassInitialize(TestContext _)
+        {
+            namespaceManager = NamespaceManager.CreateFromConnectionString(AppSetting.ServiceBusConnectionString);
+        }
+        [ClassCleanup]
+        public static void ClassCleanup()
+        {
+           
+        }
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            string queueName = Guid.NewGuid().ToString();
+            testQueue = namespaceManager.CreateQueue(queueName);
+            queueClient = QueueClient.CreateFromConnectionString(AppSetting.ServiceBusConnectionString, queueName);
+        }
+
+        [TestCleanup]
+        public void TestCleanup()
+        {
+            namespaceManager.DeleteQueue(testQueue.Path);
+        }
+
+        #endregion
+
+        [TestMethod]
+        public void Send_String_Message_Verify_Content()
+        {
+            string message = "A sample string queue message";
+            queueClient.Send(new BrokeredMessage(message));
+
+            var receivedMessage = queueClient.Receive();
+            Assert.IsTrue(receivedMessage.GetBody<string>() == message, "String Message was not correctly inserted in the queue");
+
+        }
+
+        [TestMethod]
+        public void Send_CustomObject_Message_Verify_Content()
+        {
+            var message = new Student() { Id= Guid.NewGuid().ToString(), Name = "John Doe", Age= 40 };
+            queueClient.Send(new BrokeredMessage(message));
+
+            var receivedMessage = queueClient.Receive();
+            Assert.IsTrue(receivedMessage.GetBody<Student>().Equals( message), "Custom object Message was not correctly inserted in the queue");
+        }
+
+        [TestMethod]
+        public void Send_OneStringAndOneCustomObject_Message_Verify_Content()
+        {
+            var message1 = "This is sample string message";
+            var message2 = new Student() { Id = Guid.NewGuid().ToString(), Name = "John Doe", Age = 40 };
+            queueClient.Send(new BrokeredMessage(message1));
+            queueClient.Send(new BrokeredMessage(message2));
+
+            var receivedMessage1 = queueClient.Receive();
+            var receivedMessage2 = queueClient.Receive();
+            Assert.IsTrue(receivedMessage1.GetBody<string>().Equals(message1) && receivedMessage2.GetBody<Student>().Equals(message2), "Custom object Message and String message one after another was not correctly inserted in the queue");
+        }
+    }
+}
---
+++ ServiceBusTest/ServiceBusTest/Student.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceBusTest
+{
+    public class Student
+    {
+        public string Id { get; set; }
+
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+
+        public override int GetHashCode()
+        {
+            return this.Age + this.Id.GetHashCode() + this.Name.GetHashCode();
+        }
+
+        public override bool Equals(object obj)
+        {
+            var secondStudent = obj as Student;
+            if (secondStudent == null) return false;
+            if (secondStudent == this) return true;
+
+            return this.Id == secondStudent.Id && this.Name == secondStudent.Name && this.Age == secondStudent.Age;
+        }
+    }
+}
