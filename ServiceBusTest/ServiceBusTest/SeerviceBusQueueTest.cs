9e1ef876f97fe613eb1075019cc7cf24a1a77a24
Sanjay Singh <itssanjay@gmail.com>
2015-07-09 23:07:58 -0400

    Session queue tests

--- ServiceBusTest/ServiceBusTest/SeerviceBusQueueTest.cs
+++ ServiceBusTest/ServiceBusTest/SeerviceBusQueueTest.cs
@@ -11,7 +11,7 @@ namespace ServiceBusTest
     [TestClass]
     public class SeerviceBusQueueTest
     {
-        private static QueueDescription testQueue;
+        private QueueDescription testQueue;
         private static NamespaceManager namespaceManager;
         private QueueClient queueClient;
 
@@ -25,7 +25,7 @@ namespace ServiceBusTest
         [ClassCleanup]
         public static void ClassCleanup()
         {
-           
+
         }
         [TestInitialize]
         public void TestInitialize()
@@ -57,11 +57,11 @@ namespace ServiceBusTest
         [TestMethod]
         public void Send_CustomObject_Message_Verify_Content()
         {
-            var message = new Student() { Id= Guid.NewGuid().ToString(), Name = "John Doe", Age= 40 };
+            var message = new Student() { Id = Guid.NewGuid().ToString(), Name = "John Doe", Age = 40 };
             queueClient.Send(new BrokeredMessage(message));
 
             var receivedMessage = queueClient.Receive();
-            Assert.IsTrue(receivedMessage.GetBody<Student>().Equals( message), "Custom object Message was not correctly inserted in the queue");
+            Assert.IsTrue(receivedMessage.GetBody<Student>().Equals(message), "Custom object Message was not correctly inserted in the queue");
         }
 
         [TestMethod]
@@ -98,7 +98,7 @@ namespace ServiceBusTest
             Assert.IsTrue(receivedMessage.MessageId == brokeredMessage.MessageId, "Message Id is not received correctly");
 
             Assert.IsTrue(receivedMessage.To == brokeredMessage.To, "'To' is not received correctly");
-            
+
         }
 
         [TestMethod]
@@ -124,20 +124,21 @@ namespace ServiceBusTest
 
             var receivedMessage = queueClient.Receive();
             receivedMessage = queueClient.Receive(TimeSpan.FromSeconds(2));
-            
+
             Assert.IsTrue(receivedMessage == null, "Receieved message on 2nd read should be null when only one message was in the queue");
         }
 
         [TestMethod]
-        public void Send_Multiple_Message_Verify_Subscriber_ReceivesAll()
+        public void Send_Multiple_Messages_Verify_Subscriber_ReceivesAll()
         {
             int messageCount = 10;
             List<string> receivedMessages = new List<string>();
             ManualResetEvent waitEvent = new ManualResetEvent(false);
-            queueClient.OnMessage((message) => {
+            queueClient.OnMessage((message) =>
+            {
                 receivedMessages.Add(message.GetBody<string>());
                 message.Complete();
-                if(receivedMessages.Count >= messageCount)
+                if (receivedMessages.Count >= messageCount)
                 {
                     waitEvent.Set();
                 }
@@ -151,5 +152,75 @@ namespace ServiceBusTest
             waitEvent.WaitOne(10000);
             Assert.IsTrue(Enumerable.SequenceEqual(sentMessages, receivedMessages), "Sent and received messages did not match when received using subscription");
         }
+
+        [TestMethod]
+        public void Send_Three_Sessions_Should_Reach_Correct_Subscriber()
+        {
+            var sessionQueue = namespaceManager.CreateQueue(new QueueDescription(Guid.NewGuid().ToString()) { RequiresSession = true });
+            var sessionQueueClient = QueueClient.CreateFromConnectionString(AppSetting.ServiceBusConnectionString, sessionQueue.Path);
+            List<string> session1Data = new List<string>() { "Session1-1", "Session1-2", "Session1-3" };
+            List<string> session2Data = new List<string>() { "Session2-1", "Session2-2", "Session3-3" };
+            List<string> session3Data = new List<string>() { "Session3-1", "Session3-2", "Session3-3" };
+            List<string> receivedSession1Data = new List<string>();
+            List<string> receivedSession2Data = new List<string>();
+            List<string> receivedSession3Data = new List<string>();
+            ManualResetEvent session1WaitEvent = new ManualResetEvent(false);
+            ManualResetEvent session2WaitEvent = new ManualResetEvent(false);
+            ManualResetEvent session3WaitEvent = new ManualResetEvent(false);
+
+            string sessionId1 = Guid.NewGuid().ToString();
+            string sessionId2 = Guid.NewGuid().ToString();
+            string sessionId3 = Guid.NewGuid().ToString();
+
+            var messageSession1 = sessionQueueClient.AcceptMessageSession(sessionId1);
+            messageSession1.OnMessage((message)=> {
+                receivedSession1Data.Add(message.GetBody<string>());
+                if(receivedSession1Data.Count >= session1Data.Count)
+                {
+                    session1WaitEvent.Set();
+                }
+            }, new OnMessageOptions());
+
+            var messageSession2 = sessionQueueClient.AcceptMessageSession(sessionId2);
+            messageSession2.OnMessage((message) => {
+                receivedSession2Data.Add(message.GetBody<string>());
+                if (receivedSession2Data.Count >= session2Data.Count)
+                {
+                    session2WaitEvent.Set();
+                }
+            }, new OnMessageOptions());
+
+            var messageSession3 = sessionQueueClient.AcceptMessageSession(sessionId3);
+            messageSession3.OnMessage((message) => {
+                receivedSession3Data.Add(message.GetBody<string>());
+                if (receivedSession3Data.Count >= session3Data.Count)
+                {
+                    session3WaitEvent.Set();
+                }
+            }, new OnMessageOptions());
+
+            for (int i = 0; i < session1Data.Count; i++)
+            {
+                sessionQueueClient.Send(new BrokeredMessage(session1Data[i]) { SessionId = sessionId1 });
+                sessionQueueClient.Send(new BrokeredMessage(session2Data[i]) { SessionId = sessionId2 });
+                sessionQueueClient.Send(new BrokeredMessage(session3Data[i]) { SessionId = sessionId3 });
+            }
+            
+            session1WaitEvent.WaitOne(5000);
+            session2WaitEvent.WaitOne(5000);
+            session3WaitEvent.WaitOne(5000);
+
+            bool session1DataReceived = Enumerable.SequenceEqual(session1Data, receivedSession1Data);
+            bool session2DataReceived = Enumerable.SequenceEqual(session2Data, receivedSession2Data);
+            bool session3DataReceived = Enumerable.SequenceEqual(session3Data, receivedSession3Data);
+
+            if (sessionQueue != null)
+            {
+                namespaceManager.DeleteQueue(sessionQueue.Path);
+            }
+
+            Assert.IsTrue(session1DataReceived && session2DataReceived && session3DataReceived, "When sending interleaved data on session queue, data is not correctly received in OnMessage handlers.");
+        }
+
     }
 }
