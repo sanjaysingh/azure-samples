224e90dd3f63e49d5111c64772f9c9fa12dbd9fd
Sanjay Singh <itssanjay@gmail.com>
2015-07-19 11:54:46 -0400

    event hb test project

---
+++ EventHubTest/EventHubTest/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace EventHubTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
--- ServiceBusTest/ServiceBusTest/AppSetting.cs
+++ ServiceBusTest/ServiceBusTest/AppSetting.cs
@@ -8,8 +8,8 @@ namespace ServiceBusTest
 {
     public static class AppSetting
     {
-        public const string Namespace = "";
-        public const string RootAccessKey = "";
+        public const string Namespace = "sanjaygeicons";
+        public const string RootAccessKey = "ef7ytYlkXvCCKGFFhaCFhrAx4SFob6r2GG9oBYPfBqI=";
         public static readonly string ServiceBusConnectionString = $"Endpoint=sb://{AppSetting.Namespace}.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey={AppSetting.RootAccessKey}";
     }
 }
